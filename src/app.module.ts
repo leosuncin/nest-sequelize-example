621fd409f7933ff4b042575d7f61729451ccffd1
Jaime Leonardo Suncin Cruz <jaime.suncin@inabve.gob.sv>
2023-02-06 12:49:28 -0600

    feat: setup sequelize connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      database: 'contacts.sqlite',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
